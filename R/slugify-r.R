f1919aa8827f4e762eb54fa0d66d75a73dd4c74a
boB Rudis <bob@rud.is>
2021-05-17 13:56:27 -0400

    Update slugify-r.R

--- R/slugify-r.R
+++ R/slugify-r.R
@@ -6,7 +6,7 @@
 #' @param repl what to replace spaces with
 #' @param lower lowercase final output?
 #' @export
-slugify_native <- function(x, repl = "-", lower = TRUE) {
+slugify <- function(x, repl = "-", lower = TRUE) {
 
   x <- stri_replace_all_fixed(x, names(slugify_charmap), slugify_charmap, vectorize_all = FALSE)
   x <- stri_replace_all_regex(x, "[^\\P{P}-]", "")
